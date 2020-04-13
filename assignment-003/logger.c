f70ca87db4e412a0638cd12d458a4de9a2d9bd54
Anand Chavan <anand2431997@gmail.com>
2020-04-13 16:30:14 +0530

    some changes in function

--- assignment-003/logger.c
+++ assignment-003/logger.c
@@ -7,25 +7,7 @@
 FILE *fp ;
 static int SESSION_TRACKER; //Keeps track of session
 
-char* print_time()
-{
-    int size = 0;
-    time_t t;
-    char *buf;
-    
-    t=time(NULL); /* get current calendar time */
-    
-    char *timestr = asctime( localtime(&t) );
-    timestr[strlen(timestr) - 1] = 0;  //Getting rid of \n
-    
-    size = strlen(timestr)+ 1 + 2; //Additional +2 for square braces
-    buf = (char*)malloc(size);
-    
-    memset(buf, 0x0, size);
-    snprintf(buf,size,"[%s]", timestr);
-   
-    return buf;
-}
+
 void log_print(char* filename, int line, char *fmt,...)
 {
     va_list         list;
@@ -37,7 +19,7 @@ void log_print(char* filename, int line, char *fmt,...)
     else
       fp = fopen ("log.txt","w");
     
-    fprintf(fp,"%s ",print_time());
+
     fprintf(fp,"[%s][line: %d] ",filename,line);
     va_start( list, fmt );
 
--- assignment-003/test.c
+++ assignment-003/test.c
@@ -3,13 +3,6 @@
 int main()
 {
   
-  int x = 199,i=0;
-  char *s = "Bitter Truth";
-
-  while(i<5)
-  {
-   LOG_PRINT("Hello World ");
-   LOG_PRINT("Zing is back !!! %s %d",s,x++);
-   i++;
-  }
+ LOG_PRINT("Welocme Anand Chavan !!!");
+  
 }
